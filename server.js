e0fc2616c8055fa35da3f87e7266c7f699f324b2
Yining Shi <ys2189@nyu.edu>
2017-02-04 17:15:27 -0500

    added basic authentication, submit form and changed view engine to pug

--- server.js
+++ server.js
@@ -1,32 +1,71 @@
-
 'use strict'
 
 const express = require('express')
-const app     = express()
-const msg     = require('gulp-messenger')
-const chalk   = require('chalk')
-const Twig    = require('twig')
-const _       = require('lodash')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const msg = require('gulp-messenger')
+const chalk = require('chalk')
+const _ = require('lodash')
 
-var twig = Twig.twig;
+const app = express()
 
-// This section is optional and used to configure twig.
-app.set('twig options', {
-    strict_variables: false
-});
+const sess = session({
+  isAuthenticated: false,
+  secret: 'dragon',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    maxAge: 60 * 60 * 1000,
+  },
+})
 
+app.set('view engine', 'pug')
 app.set('port', (process.env.PORT || 3000))
-app.use(express.static(__dirname + '/public'))  // static directory
 
-// Custom routes (not convered by static files, uses twig template engine)
+app.use(sess)
+app.use(express.static(__dirname + '/public'))  // static directory
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const requireLogin = (req, res, next) => {
+  if (req.session.isAuthenticated) {
+    next() // allow the next route to run
+  } else {
+    // require the user to log in
+    res.redirect('/login'); // or render a form, etc.
+  }
+}
+
+app.get('/login', (req, res) => {
+  res.render('login.pug')
+})
+
+app.post('/login', (req, res) => {
+  if (req.body.password === 'asd') {
+    req.session.isAuthenticated = true
+    res.redirect('/')
+  } else {
+    req.session.isAuthenticated = false
+    res.status(403).send('Unauthorized!')
+  }
+})
+
+app.use('/', requireLogin)
 app.get('/', (req, res) => {
-  res.render('index.twig', {
-    firstName: 'Mike',
-    lastName: 'Erickson'
+  res.render('index.pug', {
+    status: '',
   });
 });
 
-// lets startup this puppy
+app.use('/reblog-post', requireLogin)
+app.post('/reblog-post', (req, res) => {
+  res.render('index.pug', {
+    status: _.sample(['reblogged!', 'failed!']),
+  })
+})
+
+// start listening
 app.listen(app.get('port'), () => {
   msg.log('\n')
   console.log(chalk.cyan('Server Started ' + new Date()));
